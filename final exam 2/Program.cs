7a0066fc20dc8c7885a3f86bca0e293b305e4c38
Mehrdad Samadie <mehrdad_samadie@yahoo,com>
2021-01-18 17:59:16 -0500

    first commit

---
+++ final exam 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace final_exam_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
